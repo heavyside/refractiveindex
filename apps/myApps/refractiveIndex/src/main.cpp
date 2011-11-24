68170b47ef0a9a1e79aecfb1ce97addbe3dfcb68
dviid <dviid@ciid.dk>
2011-11-24 23:31:28 +0100

    refactoring
    + Abstract Analysis
    + IResponseAnalysis skeleton

--- apps/myApps/refractiveIndex/src/main.cpp
+++ apps/myApps/refractiveIndex/src/main.cpp
@@ -1,9 +1,10 @@
 #include "ofAppGlutWindow.h"
-#include "testApp.h"
+//#include "testApp.h"
+#include "RefractiveIndex.h"
 
 int main() {
 	ofAppGlutWindow window;
     //window.setGlutDisplayString("rgba double samples >= 8");
 	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
-	ofRunApp(new testApp());
+	ofRunApp(new RefractiveIndex());
 }
